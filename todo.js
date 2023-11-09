57c3b46e1a0702a130fc889a6e30403425d0cd69
bedeschiotavia <bedeschi.otavia@gmail.com>
2023-11-09 15:47:07 +0000

    Finished todo app

---
+++ todo.js
@@ -0,0 +1,23 @@
+document.addEventListener ('DOMContentLoaded', function(){
+    let ourForm = document.getElementById("ourForm")
+    let ourField = document.getElementById("ourField")
+    let ourList = document.getElementById("ourList")
+
+    ourForm.addEventListener("submit", (e) => {
+      e.preventDefault()
+      createItem(ourField.value)
+    })
+
+  function createItem (item) {
+    let ourHTML = `<li>${item}<button onclick="deleteItem(this)"class="delete btn btn-primary btn-sm">DONE!</button></li>`
+    ourList.insertAdjacentHTML("beforeend", ourHTML)
+    ourField.value = ""
+    ourField.focus()
+  }
+
+
+
+})
+function deleteItem(elementToDelete) {
+    elementToDelete.parentElement.remove()
+}
